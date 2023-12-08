009204b5b3b272da426bfadcc9a580ab914ca330
cemkaygısız <149940447+cemm361636@users.noreply.github.com>
2023-12-08 23:43:49 +0300

    Stok Takip Günceklleme Yapıldı

---
+++ WindowsFormsApp3EFDBFirst/Form1.Designer.cs
@@ -0,0 +1,298 @@
+﻿namespace WindowsFormsApp3EFDBFirst
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtAciklama = new System.Windows.Forms.RichTextBox();
+            this.txtFiyat = new System.Windows.Forms.TextBox();
+            this.cbDurum = new System.Windows.Forms.CheckBox();
+            this.txtUrunAdi = new System.Windows.Forms.TextBox();
+            this.btnGeri = new System.Windows.Forms.Button();
+            this.txtBarkod = new System.Windows.Forms.TextBox();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.txtStok = new System.Windows.Forms.TextBox();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dgvStokKontrol = new System.Windows.Forms.DataGridView();
+            this.btnAra = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtAra = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStokKontrol)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtAciklama);
+            this.groupBox1.Controls.Add(this.txtFiyat);
+            this.groupBox1.Controls.Add(this.cbDurum);
+            this.groupBox1.Controls.Add(this.txtUrunAdi);
+            this.groupBox1.Controls.Add(this.btnGeri);
+            this.groupBox1.Controls.Add(this.txtBarkod);
+            this.groupBox1.Controls.Add(this.btnSil);
+            this.groupBox1.Controls.Add(this.txtStok);
+            this.groupBox1.Controls.Add(this.btnGuncelle);
+            this.groupBox1.Controls.Add(this.btnEkle);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(689, 66);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(409, 453);
+            this.groupBox1.TabIndex = 23;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ürün Bilgileri";
+            // 
+            // txtAciklama
+            // 
+            this.txtAciklama.Location = new System.Drawing.Point(85, 221);
+            this.txtAciklama.Name = "txtAciklama";
+            this.txtAciklama.Size = new System.Drawing.Size(100, 96);
+            this.txtAciklama.TabIndex = 21;
+            this.txtAciklama.Text = "";
+            // 
+            // txtFiyat
+            // 
+            this.txtFiyat.Location = new System.Drawing.Point(80, 174);
+            this.txtFiyat.Name = "txtFiyat";
+            this.txtFiyat.Size = new System.Drawing.Size(100, 22);
+            this.txtFiyat.TabIndex = 6;
+            this.txtFiyat.TextChanged += new System.EventHandler(this.txtFiyat_TextChanged);
+            // 
+            // cbDurum
+            // 
+            this.cbDurum.AutoSize = true;
+            this.cbDurum.Location = new System.Drawing.Point(137, 329);
+            this.cbDurum.Name = "cbDurum";
+            this.cbDurum.Size = new System.Drawing.Size(54, 20);
+            this.cbDurum.TabIndex = 20;
+            this.cbDurum.Text = "Aktif\r\n";
+            this.cbDurum.UseVisualStyleBackColor = true;
+            // 
+            // txtUrunAdi
+            // 
+            this.txtUrunAdi.Location = new System.Drawing.Point(80, 21);
+            this.txtUrunAdi.Name = "txtUrunAdi";
+            this.txtUrunAdi.Size = new System.Drawing.Size(100, 22);
+            this.txtUrunAdi.TabIndex = 3;
+            // 
+            // btnGeri
+            // 
+            this.btnGeri.Location = new System.Drawing.Point(328, 358);
+            this.btnGeri.Name = "btnGeri";
+            this.btnGeri.Size = new System.Drawing.Size(75, 23);
+            this.btnGeri.TabIndex = 18;
+            this.btnGeri.Text = "Geri";
+            this.btnGeri.UseVisualStyleBackColor = true;
+            this.btnGeri.Click += new System.EventHandler(this.btnGeri_Click);
+            // 
+            // txtBarkod
+            // 
+            this.txtBarkod.Location = new System.Drawing.Point(80, 72);
+            this.txtBarkod.Name = "txtBarkod";
+            this.txtBarkod.Size = new System.Drawing.Size(100, 22);
+            this.txtBarkod.TabIndex = 4;
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(229, 358);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(75, 23);
+            this.btnSil.TabIndex = 17;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // txtStok
+            // 
+            this.txtStok.Location = new System.Drawing.Point(80, 123);
+            this.txtStok.Name = "txtStok";
+            this.txtStok.Size = new System.Drawing.Size(100, 22);
+            this.txtStok.TabIndex = 5;
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Location = new System.Drawing.Point(107, 358);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(105, 23);
+            this.btnGuncelle.TabIndex = 16;
+            this.btnGuncelle.Text = "Güncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(26, 358);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(75, 23);
+            this.btnEkle.TabIndex = 15;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 16);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Ürün Adı";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(11, 224);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(63, 16);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Açıklama";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 16);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Barkod";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(38, 175);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 16);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Fiyat";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 16);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Stok";
+            // 
+            // dgvStokKontrol
+            // 
+            this.dgvStokKontrol.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvStokKontrol.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvStokKontrol.Location = new System.Drawing.Point(12, 66);
+            this.dgvStokKontrol.Name = "dgvStokKontrol";
+            this.dgvStokKontrol.RowHeadersWidth = 51;
+            this.dgvStokKontrol.RowTemplate.Height = 24;
+            this.dgvStokKontrol.Size = new System.Drawing.Size(642, 493);
+            this.dgvStokKontrol.TabIndex = 22;
+            this.dgvStokKontrol.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvStokKontrol_CellClick);
+            // 
+            // btnAra
+            // 
+            this.btnAra.Location = new System.Drawing.Point(241, 22);
+            this.btnAra.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAra.Name = "btnAra";
+            this.btnAra.Size = new System.Drawing.Size(100, 28);
+            this.btnAra.TabIndex = 26;
+            this.btnAra.Text = "Ara";
+            this.btnAra.UseVisualStyleBackColor = true;
+            this.btnAra.Click += new System.EventHandler(this.btnAra_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(25, 27);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(59, 16);
+            this.label6.TabIndex = 24;
+            this.label6.Text = "Ürün Ara";
+            // 
+            // txtAra
+            // 
+            this.txtAra.Location = new System.Drawing.Point(98, 27);
+            this.txtAra.Margin = new System.Windows.Forms.Padding(4);
+            this.txtAra.Name = "txtAra";
+            this.txtAra.Size = new System.Drawing.Size(132, 22);
+            this.txtAra.TabIndex = 25;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1110, 631);
+            this.Controls.Add(this.btnAra);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtAra);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dgvStokKontrol);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Stok Kontrol ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStokKontrol)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RichTextBox txtAciklama;
+        private System.Windows.Forms.TextBox txtFiyat;
+        private System.Windows.Forms.CheckBox cbDurum;
+        private System.Windows.Forms.TextBox txtUrunAdi;
+        private System.Windows.Forms.Button btnGeri;
+        private System.Windows.Forms.TextBox txtBarkod;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.TextBox txtStok;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dgvStokKontrol;
+        private System.Windows.Forms.Button btnAra;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtAra;
+    }
+}
+
---
+++ WindowsFormsApp3EFDBFirst/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3EFDBFirst
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        AdpnetDbEntities context = new AdpnetDbEntities();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dgvStokKontrol.DataSource = context.StokKontrol.ToList();
+
+        }
+        void Temizle()
+        {
+            txtUrunAdi.Text = string.Empty;
+            txtBarkod.Text = string.Empty;
+            txtStok.Text = string.Empty;
+            txtFiyat.Text = string.Empty;
+            txtAciklama.Text = string.Empty;
+            btnEkle.Enabled = true;
+            btnGuncelle.Enabled = false;
+            btnSil.Enabled = false;
+        }
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtUrunAdi.Text))
+            {
+                MessageBox.Show("Ürün Adı Boş Bırakılamaz!");
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(txtFiyat.Text))
+            {
+                MessageBox.Show("Ürün Fiyatı Boş Bırakılamaz!");
+                return;
+            }
+                try
+            {
+                context.StokKontrol.Add(new StokKontrol
+                {
+                    Name = txtUrunAdi.Text,
+                    Barcode = txtBarkod.Text,
+                    Stock = int.Parse(txtStok.Text),
+                    Price = Convert.ToInt32(txtFiyat.Text),
+                    Description = txtAciklama.Text,
+                    IsActive = cbDurum.Checked
+                    
+                }); 
+                var sonuc = context.SaveChanges();
+                if (sonuc > 0)
+                {
+                    Temizle();
+                    dgvStokKontrol.DataSource = context.StokKontrol.ToList();
+                    MessageBox.Show("Kayıt Başarılı!");
+                }
+                else
+                    MessageBox.Show("Kayıt Başarısız!");
+
+
+            }
+            catch (Exception Hata)
+            {
+
+                MessageBox.Show("Hata Oluştu!" + Hata);
+            }
+
+        }
+
+        private void dgvStokKontrol_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+
+            {
+
+                btnEkle.Enabled = false;
+                btnGuncelle.Enabled = true;
+                btnSil.Enabled = true;
+                btnGeri.Enabled = true;
+                
+                int StokId= Convert.ToInt32(dgvStokKontrol.CurrentRow.Cells[0].Value);
+                var stok = context.StokKontrol.Find(StokId);
+                txtUrunAdi.Text = stok.Name;
+                txtBarkod.Text = stok.Barcode.ToString();
+                txtStok.Text = stok.Stock.ToString();
+                txtFiyat.Text = stok.Price.ToString();
+                txtAciklama.Text = stok.Description;
+               
+                
+                
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Hata Oluştu!");
+            }
+        }
+
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            int StokId = Convert.ToInt32(dgvStokKontrol.CurrentRow.Cells[0].Value);
+            var stok = context.StokKontrol.Find(StokId);
+            stok.Name = txtUrunAdi.Text;
+            stok.Barcode = txtBarkod.Text;
+            stok.Stock = int.Parse(txtStok.Text);
+            stok.Price = Convert.ToInt32(txtFiyat.Text);
+            stok.Description = txtAciklama.Text;
+            var sonuc = context.SaveChanges();
+            if (sonuc > 0)
+            {
+                Temizle();
+                dgvStokKontrol.DataSource = context.StokKontrol.ToList();
+                MessageBox.Show("Kayıt Başarılı!");
+            }
+            else
+            {
+                MessageBox.Show("Kayıt Başarısız!");
+            }
+
+
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int StokId = Convert.ToInt32(dgvStokKontrol.CurrentRow.Cells[0].Value);
+                var stok = context.StokKontrol.Find(StokId);
+                context.StokKontrol.Remove(stok); // ef de silinecek olarak işaretler
+                var sonuc = context.SaveChanges();
+                if (sonuc > 0)
+                {
+                    Temizle();
+                    dgvStokKontrol.DataSource = context.StokKontrol.ToList();
+                    MessageBox.Show("Kayıt Silindi!");
+                }
+                else
+                {
+                    MessageBox.Show("Kayıt Silinemedi!");
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Hata Oluştu!");
+            }
+        }
+
+        private void btnGeri_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAra_Click(object sender, EventArgs e)
+        {
+            dgvStokKontrol.DataSource = context.Urunler.Where(u => u.Name.Contains(txtAra.Text)).ToList(); 
+        }
+
+        private void txtAra_TextChanged(object sender, EventArgs e)
+        {
+           
+            dgvStokKontrol.DataSource = context.Urunler.Where(u => u.Name.Contains(txtAra.Text) || u.Description.Contains(txtAra.Text)).ToList();
+        }
+
+        private void txtFiyat_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+    
+    
+}
